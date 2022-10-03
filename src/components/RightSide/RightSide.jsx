e670210f43f6e265a1103792d98dcd0724633642
jpriyanshjain <jpriyanshjain@gmail.com>
2022-10-03 23:24:32 +0530

    remove unused variables

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "./RightSide.css";
-import Home from "../../img/home.png";
-import Noti from "../../img/noti.png";
-import Comment from "../../img/comment.png";
-import { UilSetting } from "@iconscout/react-unicons";
 import TrendCard from "../TrendCard/TrendCard";
-import ShareModal from "../ShareModal/ShareModal";
 import NavIcons from "../NavIcons/NavIcons";
-import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 const RightSide = () => {
-  const [modalOpened, setModalOpened] = useState(false);
   const dispatch = useDispatch();
 
   const comingSoonHandler = () => {
